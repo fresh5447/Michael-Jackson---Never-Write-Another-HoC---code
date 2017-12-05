60a6d6c90618b87bd6ea2d63fa4ddaf2023272e4
fresh5447 <doug@kosmojo.com>
2017-12-05 15:15:47 -0700

    HOC example implemented with notes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ import React, {Component} from 'react'
 // We could take info that is shared, put it in the HOC, then just
 // wrap all of our components in the HOC.
 
+// 1) We can share behavior - anyone can use the withMouseComp
+
+// Problems with HOC implementation
+
+// We have a lot of the same problems we've had with Mixins,
+// weve just changed forms.
+// we can use ES6 classes, woohoo 👎
+
+// "Composition is better than inheritance"
+
 // HOF
 const add = (x, y) => x + y
 add(9, 8)
@@ -16,6 +26,28 @@ const createAdder = (a) => (b) => add(a, b) // HOF
 const addTwo = createAdder(2)
 
 addTwo(8) // 10
+
+const withCat = (Component) => {
+  return class extends Component {
+    state = {
+      x: 0,
+      y: 0
+    }
+    handleMouseMove = (event) => {
+      this.setState({
+        x: event.clientX,
+        y: event.clientY
+      })
+    }
+    render () {
+      return (
+        <div onMouseMove={this.handleMouseMove}>
+          <Component {...this.props} mouse={this.state} />
+        </div>
+      )
+    }
+  }
+}
 
 const withMouse = (Component) => {
   return class extends Component {
@@ -32,7 +64,7 @@ const withMouse = (Component) => {
     render () {
       return (
         <div onMouseMove={this.handleMouseMove}>
-          <Component mouse={this.state} />
+          <Component {...this.props} mouse={this.state} />
         </div>
       )
     }
@@ -46,10 +78,27 @@ class App extends Component {
       <div style={{height: '100vh'}}>
         <div>
           <h1> The Mouse Position is ({x} , {y})</h1>
+          <p>The message is {this.props.message} </p>
         </div>
       </div>
     )
   }
 }
 
-export default withMouse(App)
+// Composition is happening statically (doesn't change),
+// at class creation time.
+// The whole React flow is very dynamic
+// we have to be very careful up front when
+// doing things statically
+// we have to be sure that's what we want.
+// HOCS do not compose dynamically (not a failure of mixins)
+export default withMouse(withCat(App))
+
+// When App is imported, they aren't really
+// aware of the withMouse thing..
+// Have to pass through props to get message.
+
+// Still have name collision problems. See withCat example
+// multiple HOCS on the same component. Sometimes you wrap it multiple times.
+// 8 or 9 components wrapping one component.
+// every problem is the nail, and every HOC is the hammer
